b1a1ec3542e164568613cee207c49ecbb1d26efe
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-06-22 21:32:35 -0300

    ex

--- Modulo 2/Semana08/aula-react/src/components/HomeComponents/CardNoticia/CardNoticia.jsx
+++ Modulo 2/Semana08/aula-react/src/components/HomeComponents/CardNoticia/CardNoticia.jsx
@@ -1,11 +1,10 @@
 import './styles.css'
-function CardNoticia({titulo}) {
+function CardNoticia({ titulo, srcImg }) {
     return (
-        <div className="card-noticia">   
-        <img src="https://s2.glbimg.com/QxgfZZ0KaNp-BRgg_Pfile-2-WM=/0x468:1536x1332/fit-in/629x354/middle/smart/filters:strip_icc():strip_exif():format(webp)/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/3/8/IFfzYCRkGLGL7jqP5YAg/fzqkz10wwaqujd6.jfif
-        " alt="" />
-        <h3>Bitello empata para o Grêmio contra o América-MG na Arena; SIGA</h3>         
-        <span>{titulo}</span>
+        <div className="card-noticia">
+            <img
+                src={srcImg} />
+            <h3>{titulo}</h3>
         </div>
     )
 }
--- Modulo 2/Semana08/aula-react/src/pages/home/Home.jsx
+++ Modulo 2/Semana08/aula-react/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import CardNoticia from "../../components/HomeComponents/CardNoticia/CardNoticia"
 function Home() {
-    const manchete ='Este é o titulo'
+    const manchete ='Com gol de Suárez, Grêmio vira sobre o America-MG e entra no G-4'
+    const srcImg = 'https://s2.glbimg.com/QxgfZZ0KaNp-BRgg_Pfile-2-WM=/0x468:1536x1332/fit-in/629x354/middle/smart/filters:strip_icc():strip_exif():format(webp)/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/3/8/IFfzYCRkGLGL7jqP5YAg/fzqkz10wwaqujd6.jfif'
     return (
         <>
             <h1>Home</h1>
-            <CardNoticia titulo={manchete}/>
+            <CardNoticia titulo={manchete}
+            srcImg={srcImg}/>
         </>
     )
 }
